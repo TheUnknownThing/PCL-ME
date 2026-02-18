3735a9b8fc1839af6664e66800185858226a2076
薄奚梦灵 <boximengling@outlook.com>
2026-02-18 20:59:39 +0800

    perf(extension): 使用栈以提高性能
    一只小梦灵在堆上反复横跳然后不小心掉进栈里面了

--- PCL.Core/Utils/Exts/ByteExtension.cs
+++ PCL.Core/Utils/Exts/ByteExtension.cs
@@ -17,11 +17,7 @@ public static class ByteExtension
 
             return sb.ToString();
         }
-    }
-
-    extension(byte[] input)
-    {
-        public string FromByteToB64() => Convert.ToBase64String(input);
-        public string FromBytesToB64UrlSafe() => input.FromByteToB64().FromB64ToB64UrlSafe();        
+        public string FromByteToB64() => Convert.ToBase64String(bytes);
+        public string FromBytesToB64UrlSafe() => bytes.FromByteToB64().FromB64ToB64UrlSafe();
     }
 }
