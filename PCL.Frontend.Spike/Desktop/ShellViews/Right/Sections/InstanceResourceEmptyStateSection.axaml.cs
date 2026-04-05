a79d35ab0bd4d8ce747f0e468040f77f75aa0d4a
TheUnknownThing <wanghn051@outlook.com>
2026-04-06 00:51:37 +0800

    Decompose remaining heavy instance panes

---
+++ PCL.Frontend.Spike/Desktop/ShellViews/Right/Sections/InstanceResourceEmptyStateSection.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PCL.Frontend.Spike.Desktop.ShellViews.Right.Sections;
+
+internal sealed partial class InstanceResourceEmptyStateSection : UserControl
+{
+    public InstanceResourceEmptyStateSection()
+    {
+        InitializeComponent();
+    }
+}
---
+++ PCL.Frontend.Spike/Desktop/ShellViews/Right/Sections/InstanceResourceEntriesSection.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PCL.Frontend.Spike.Desktop.ShellViews.Right.Sections;
+
+internal sealed partial class InstanceResourceEntriesSection : UserControl
+{
+    public InstanceResourceEntriesSection()
+    {
+        InitializeComponent();
+    }
+}
