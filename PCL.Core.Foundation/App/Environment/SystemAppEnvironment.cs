aee5163258092812533b2ac4ebccf40204add4f0
wano <multicolor@sjtu.edu.cn>
2026-04-09 17:03:40 +0800

    Update `DefaultDirectory` property to use `AppContext.BaseDirectory` for debug

--- PCL.Core.Foundation/App/Environment/SystemAppEnvironment.cs
+++ PCL.Core.Foundation/App/Environment/SystemAppEnvironment.cs
@@ -8,8 +8,9 @@ public sealed class SystemAppEnvironment : IAppEnvironment
 {
     public static SystemAppEnvironment Current { get; } = new();
 
-    public string DefaultDirectory { get; } = Path.GetDirectoryName(Environment.ProcessPath!)
-                                              ?? Environment.CurrentDirectory;
+    public string DefaultDirectory { get; } = AppContext.BaseDirectory.TrimEnd(
+                                                  Path.DirectorySeparatorChar,
+                                                  Path.AltDirectorySeparatorChar);
     public string TempPath => Path.GetTempPath();
 
     public string GetFolderPath(Special folder)
